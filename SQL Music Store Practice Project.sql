c777d7c1fee011147fd50bf21cb41de024b99fc0
Paige Ball <paige.ball95@gmail.com>
2020-09-15 19:22:21 -0700

    uploading another query

--- SQL Music Store Practice Project.sql
+++ SQL Music Store Practice Project.sql
@@ -11,3 +11,15 @@ JOIN tracks
 GROUP BY playlist_track.TrackId
 ORDER BY COUNT(playlist_track.TrackId) DESC
 LIMIT 100;
+
+
+--(Working on it) Which track generated the most revenue? which album? which genre?
+SELECT tracks.Name AS "Song", SUM(invoice_items.UnitPrice) AS "Total Revenue"	--Selects columns from both tables and renames them for the output
+FROM invoice_items	--References the first table
+
+JOIN tracks --Join invoice_items with tracks 
+	ON invoice_items.TrackId = tracks.TrackId --Link the keys
+
+GROUP BY name --Group the results by the name of the song
+ORDER BY SUM(invoice_items.UnitPrice) DESC; --Order the results from highest revenue to lowest
+
